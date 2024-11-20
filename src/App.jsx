82d1626f3fb8803a78846bf5fbb884a62254e59b
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-11-20 23:17:16 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Main from './Pages/AdminDashboard/Mainpage/Main';
 import Orders from './Pages/AdminDashboard/Orders/Orders';
 import CreateOrder from './Pages/AdminDashboard/CreateOrder/CreateOrder';
+import MenuItems from './Pages/AdminDashboard/MenuItems/MenuItems';
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
     <Route path='/' element = {<Main />} />
     <Route path="/orders" element={<Orders />} />
     <Route path="/create-order" element={<CreateOrder />} />
+    <Route path="/menu-items" element={<MenuItems />} />
     </Routes>
    </>
   )
---
+++ src/Pages/AdminDashboard/MenuItems/MenuItems.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AdminSidebar from '../../../Components/AdminSidebar/AdminSidebar';
+
+const MenuItems = () => {
+  return (
+    <div className="flex font-outfit ">
+      <AdminSidebar />
+      <div className="lg:ml-[23%] lg:w-[77%] bg-gray-50 p-4">
+        <h1>hyy</h1>
+    </div>
+    </div>
+  );
+}
+
+
+export default MenuItems
